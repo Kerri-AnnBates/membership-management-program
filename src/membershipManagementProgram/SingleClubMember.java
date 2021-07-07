75cc80d33ccbd9327761a4a9a2ddc30d0765b71a
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-07-07 18:02:13 -0400

    create single club member class

---
+++ src/membershipManagementProgram/SingleClubMember.java
@@ -0,0 +1,23 @@
+package membershipManagementProgram;
+
+public class SingleClubMember extends Member {
+    private int club;
+
+    SingleClubMember(char memberType, int memberID, String name, double fees, int club) {
+        super(memberType, memberID, name, fees);
+        this.club = club;
+    }
+
+    public int getClub() {
+        return club;
+    }
+
+    public void setClub(int club) {
+        this.club = club;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", " + club;
+    }
+}
